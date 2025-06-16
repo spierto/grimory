a486f20bc25911baf2576519b5aa9a7aeed37df9
spierto <spiertoangela.art@gmail.com>
2025-06-16 13:01:14 +0200

    set up db

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 # db
 
+from datetime import datetime
 from . import app
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,7 +9,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db=SQLAlchemy(app)
 
-class Persona(db.Model):
+class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
-  nome = db.Column(db.String(100), nullable = False)
-  anni = db.Column(db.Integer)
+  name = db.Column(db.String(100), nullable = False)
+  email = db.Column(db.String(100), unique = True, nullable = False)
+  pages = db.relationship('Page', backref = 'author', lazy = True)
+
+class Page(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  title = db.Column(db.String(200), nullable = False)
+  author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+  text = db.Column(db.Text, nullable = False)
+  date = db.Column(db.DateTime, default=datetime.now())
